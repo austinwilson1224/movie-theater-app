b4fd3341e91660ef550ca0c0b485fa25f6f48b5a
austinwilson1224 <austin1224@gmail.com>
2020-07-13 21:44:03 -0700

    finished movie theater app, may still be some bugs

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
 // + is to turn string into number
 let ticketPrice = +movieSelect.value;
+
+// save selected movie index and price
+function setMovieData(movieIndex,moviePrice) {
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedPrice',moviePrice)
+}
 
 // update total count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    // copy selected seats into arr
+    // map through array
+    // return new array indexes 
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    // console.log(seatsIndex)
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     // console.log(selectedSeats);
     const selectedSeatsCount = selectedSeats.length;
     // console.log(selectedSeatsCount)
@@ -17,11 +32,30 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-// console.log(ticketPrice);
+
+// get data from storage and populate the UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    //console.log(selectedSeats)
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat,index) => {
+            if(selectedSeats.indexOf(index)) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+}
 
 
 // seat click event
-container.addEventListener('click', function(e) {
+container.addEventListener('click', e => {
     if(
         e.target.classList.contains('seat') && 
         !e.target.classList.contains('occupied')
@@ -34,8 +68,13 @@ container.addEventListener('click', function(e) {
 
 // movie select event 
 movieSelect.addEventListener('change', e => {
-    ticketPrice = e.target.value;
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex,e.target.value);
     updateSelectedCount();
-})
+});
+
+
+// initial count and total set 
+updateSelectedCount();
 
 
